ab07503cecac2a85f93503ec68fecb77c1b88c69
Sanjid Rahman <sanjid.rahman@servian.com>
2022-12-04 15:14:39 +1300

    solve 2022 day2

---
+++ 2022/R/day02.R
@@ -0,0 +1,40 @@
+# Preprocessing
+dta <- data.table::fread(
+  "C:/Users/SanjidRahman/projects/advent-of-code/2022/data/day-02.txt", 
+  header = FALSE)[, .(Opponent = V1, Part1_Me = V2)]
+
+# PART 1
+dta[Opponent == "A" & Part1_Me == "X", `:=`(Score = 3, Shape = 1)]
+dta[Opponent == "A" & Part1_Me == "Y", `:=`(Score = 6, Shape = 2)] #
+dta[Opponent == "A" & Part1_Me == "Z", `:=`(Score = 0, Shape = 3)] 
+
+dta[Opponent == "B" & Part1_Me == "X", `:=`(Score = 0, Shape = 1)] # 
+dta[Opponent == "B" & Part1_Me == "Y", `:=`(Score = 3, Shape = 2)] #
+dta[Opponent == "B" & Part1_Me == "Z", `:=`(Score = 6, Shape = 3)] #
+
+dta[Opponent == "C" & Part1_Me == "X", `:=`(Score = 6, Shape = 1)] #
+dta[Opponent == "C" & Part1_Me == "Y", `:=`(Score = 0, Shape = 2)] #
+dta[Opponent == "C" & Part1_Me == "Z", `:=`(Score = 3, Shape = 3)] #
+
+dta[, Total_Score := Score + Shape]
+
+message("Part 1 Answer: ", sum(dta$Total_Score))
+# PART 1 Answer: 10816
+
+# PART 2
+dta[, Part2_Outcome := Part1_Me]
+dta[Opponent == "A" & Part2_Outcome == "X", `:=`(Score = 0, Shape = 3)]
+dta[Opponent == "A" & Part2_Outcome == "Y", `:=`(Score = 3, Shape = 1)] #
+dta[Opponent == "A" & Part2_Outcome == "Z", `:=`(Score = 6, Shape = 2)] 
+
+dta[Opponent == "B" & Part2_Outcome == "X", `:=`(Score = 0, Shape = 1)] # 
+dta[Opponent == "B" & Part2_Outcome == "Y", `:=`(Score = 3, Shape = 2)] #
+dta[Opponent == "B" & Part2_Outcome == "Z", `:=`(Score = 6, Shape = 3)] #
+
+dta[Opponent == "C" & Part2_Outcome == "X", `:=`(Score = 0, Shape = 2)] #
+dta[Opponent == "C" & Part2_Outcome == "Y", `:=`(Score = 3, Shape = 3)] #
+dta[Opponent == "C" & Part2_Outcome == "Z", `:=`(Score = 6, Shape = 1)] #
+
+dta[, Total_Score := Score + Shape]
+message("Part 2 Answer: ", sum(dta$Total_Score))
+# PART 2 Answer: 11657
