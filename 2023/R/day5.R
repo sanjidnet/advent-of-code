aa6882515a70cdf8fd63a89eed43a046cad321e0
sanjidnet <contact@sanjid.net>
2023-12-06 10:43:57 +1300

    resolve 2023 day 5 part 1 with break

--- 2023/R/day5.R
+++ 2023/R/day5.R
@@ -8,17 +8,12 @@ for (each_seed in seeds) {
       message(dta[i])
       i <- i + 1
       while (dta[i] != "") {
-        
-        # Code passes with sample data only except the below condition
-        if(each_seed == 14 & i == 14){ i = i + 1; next}
-        # where seed is 14 and almanac rule is:         
-        # 0 11 42
-        
         map_line <- as.numeric(unlist(strsplit(dta[i], " ")))
         if(seeds[seed_count] >= map_line[2] & seeds[seed_count] < map_line[2] + map_line[3]){
           seeds[seed_count] <- seeds[seed_count] + map_line[1] - map_line[2]
+          break
         } 
-        message("seed: ", each_seed, " row: ", i, " transformed to: ", seeds[seed_count])
+        # message("seed: ", each_seed, " row: ", i, " transformed to: ", seeds[seed_count])
         i <- i + 1
       }
     }
@@ -27,3 +22,4 @@ for (each_seed in seeds) {
 }
 
 message(paste(seeds, collapse = "-"))
+message(min(seeds))
