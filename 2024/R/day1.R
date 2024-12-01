067ab1504c286666b5a42a24c2cab80ee689997e
sanjidnet <contact@sanjid.net>
2024-12-01 18:25:15 +1300

    solve 2024 day 1 part 2

--- 2024/R/day1.R
+++ 2024/R/day1.R
@@ -4,4 +4,11 @@ dta <- data.table::fread(paste0("../data/day1/input"), header = FALSE)
 
 output <- data.table(cbind(sort(dta[,V1]), sort(dta[, V2])))
 output[, diff := abs(V2 - V1)]
-sum(output$diff)
+message("Part 1: ", sum(output$diff))
+
+# Frequency table for part 2
+frequency_table <- output[, .N, by = V2]
+# JOIN frequency table to actual table
+output2 <- merge(output, frequency_table, by.x = "V1", by.y = "V2")
+output2[, product := V1 * N]
+message("Part 2: ", sum(output2$product))
